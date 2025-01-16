7832888597ff928eb1c6f9966e7093d602418ea1
Arman Hingora <armanhingora555@gmail.com>
2025-01-16 20:36:40 +0530

    ReactJS

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Check from "./Check";
+
+function App() {
+  return (
+    <Check/>
+  );
+}
+
+export default App;
---
+++ src/Check.js
@@ -0,0 +1,8 @@
+function Check(){
+    return(
+        <>
+            <h1>Arman With React</h1>
+        </>
+    )
+}
+export default Check;
