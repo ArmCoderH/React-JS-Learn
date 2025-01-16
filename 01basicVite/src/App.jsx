8710d4cdc97caa6348c5470f212d2356fb09dbd1
Arman Hingora <armanhingora555@gmail.com>
2025-01-17 00:13:11 +0530

    project01

--- 01basicVite/src/App.jsx
+++ 01basicVite/src/App.jsx
@@ -5,9 +5,13 @@ function App() {
   return (
 
     <>
+
       <h1>Arman with react + Vite</h1>
+
       <Check/>
+      
     </>
   )
 }
+
 export default App
--- 01basicVite/src/main.jsx
+++ 01basicVite/src/main.jsx
@@ -2,8 +2,33 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+const anootherELement =(
+  <a href='https://google.com' target='_blank'> Click on google</a>
 )
+
+//New Element Declair
+
+function myApp(){
+  return(
+    <div>
+      <h1>Arman Hingora</h1>
+    </div>
+  )
+}
+
+// createRoot(document.getElementById('root'))
+// .render(
+//   anootherELement
+// )
+
+createRoot(document.getElementById('root'))
+.render(
+
+  myApp()
+
+)
+
+// createRoot(document.getElementById('root')).render(
+//     <App />
+// )
